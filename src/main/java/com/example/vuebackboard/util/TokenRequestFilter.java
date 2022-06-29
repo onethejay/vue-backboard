c43c5afee321f3f4ad630f4a74294520810bfed4
onethejay <jhjang951@gmail.com>
2022-06-29 22:16:00 +0900

    토큰 필터 수정

--- src/main/java/com/example/vuebackboard/util/TokenRequestFilter.java
+++ src/main/java/com/example/vuebackboard/util/TokenRequestFilter.java
@@ -60,7 +60,7 @@ public class TokenRequestFilter extends OncePerRequestFilter {
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7, headerAuth.length());
+            return headerAuth.substring(7);
         }
         return null;
     }
