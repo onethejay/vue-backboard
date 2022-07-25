fd0916417b7e0dea0002c2b64c0185a17c18fecf
onethejay <jhjang951@gmail.com>
2022-07-26 00:04:34 +0900

    옵셔널

--- src/test/java/com/example/vuebackboard/util/StreamTest.java
+++ src/test/java/com/example/vuebackboard/util/StreamTest.java
@@ -3,11 +3,8 @@ package com.example.vuebackboard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +12,7 @@ class StreamTest {
 
     @DisplayName("1. 스트림 생성")
     @Test
-    void test_1(){
+    void test_1() {
         List<Map<String, Object>> mapList = new ArrayList<>();
 
         mapList.stream();        //List Collection의 .stream() 메서드를 통해 스트림을 생성한다.
@@ -23,7 +20,7 @@ class StreamTest {
 
     @DisplayName("2. 스트림 중간연산 - filter")
     @Test
-    void test_2(){
+    void test_2() {
         List<Map<String, Object>> mapList = new ArrayList<>();
         Map<String, Object> amountMap;
 
@@ -65,7 +62,7 @@ class StreamTest {
 
     @DisplayName("3. 스트림 중간연산 - map")
     @Test
-    void test_3(){
+    void test_3() {
         List<Map<String, Object>> mapList = new ArrayList<>();
         Map<String, Object> amountMap;
 
@@ -84,10 +81,79 @@ class StreamTest {
         amountMap.put("amount", 240000);
         mapList.add(amountMap);
 
-        Map<String, Object> krwMap = mapList.stream()
-                .filter(m -> "KRW".equals(m.get("currencyCd")))     //mapList에서 currencyCd가 "KRW"인 객체가 필터링  (Stream)
+        int streamKrwAmount = (int) mapList.stream()
+                .filter(m -> "KRW".equals(m.get("currencyCd")))     //mapList에서 currencyCd가 "KRW"인 객체가 필터링 - 중간연산 (Stream)
+                .map(m -> m.get("amount"))
                 .findFirst()                                        //필터링된 Stream에서 객체를 찾아 꺼낸다. - 최종연산 (Optional<Map<String, Object>>)
                 .get();                                             //Optional 객체에서 데이터를 꺼내 krwMap에 할당한다.
-        int streamKrwAmount = (int) krwMap.get("amount");
+
+
+    }
+
+    @DisplayName("4. 스트림 최종연산 - collect")
+    @Test
+    void test_4() {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map<String, Object> amountMap;
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "KRW");
+        amountMap.put("amount", 120000);
+        mapList.add(amountMap);
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "USD");
+        amountMap.put("amount", 100);
+        mapList.add(amountMap);
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "CNY");
+        amountMap.put("amount", 240000);
+        mapList.add(amountMap);
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "KRW");
+        amountMap.put("amount", 320000);
+        mapList.add(amountMap);
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "USD");
+        amountMap.put("amount", 99900);
+        mapList.add(amountMap);
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "CNY");
+        amountMap.put("amount", 721828);
+        mapList.add(amountMap);
+
+        List<String> currencyList = mapList.stream()
+                .map(m -> (String) m.get("currencyCd"))
+                .collect(Collectors.toList());
+
+        Set<String> currencySet = mapList.stream()
+                .map(m -> (String) m.get("currencyCd"))
+                .collect(Collectors.toSet());
+
+        System.out.println(currencyList);
+        System.out.println(currencySet);
+    }
+
+    @DisplayName("5. 스트림 Optional")
+    @Test
+    void test_5(){
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map<String, Object> amountMap;
+
+        amountMap = new HashMap<>();
+        amountMap.put("currencyCd", "KRW");
+        amountMap.put("amount", null);
+        mapList.add(amountMap);
+
+        int amount = (int) mapList.stream()
+                .map(m -> m.get("amount"))
+                .findFirst()
+                .get();
+
+        System.out.println(amount);
     }
 }
